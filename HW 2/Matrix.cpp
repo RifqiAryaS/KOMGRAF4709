f02466789fd212243aeaa9c5bf5a2cd2a75f0f01
Shiro Kaslana <rifqiaryasaputra61@gmail.com>
2023-09-27 22:06:23 +0700

    Penambahan GUI ke HW 2

--- HW 2/Matrix.cpp
+++ HW 2/Matrix.cpp
@@ -54,6 +54,7 @@ void perkalianInput(int matrix1[2][2])
     {
         for (int j = 0; j < 2; j++)
         {
+
             cin >> input[i][j];
         }
     }
@@ -110,10 +111,48 @@ int main()
 {
     int matrix1[2][2] = {{1, 3}, {2, 4}};
     int matrix2[2][2] = {{1, 1}, {0, 0}};
+    int pilihan;
 
-    pertambahan(matrix1, matrix2);
-    // perkurangan(matrix1, matrix2);
-    // perkalianInput(matrix1);
-    perkalian(matrix1, matrix2);
+    do
+    {
+        cout << "1. Pertambahan\n";
+        cout << "2. Pengurangan\n";
+        cout << "3. Perkalian dengan input\n";
+        cout << "4. Perkalian\n";
+        cout << "5. Exit\n";
+        cout << "Masukkan Pilihan : ";
+        cin >> pilihan;
+
+        if (pilihan == 1)
+        {
+            pertambahan(matrix1, matrix2);
+        }
+
+        else if (pilihan == 2)
+        {
+            perkurangan(matrix1, matrix2);
+        }
+
+        else if (pilihan == 3)
+        {
+            perkalianInput(matrix1);
+        }
+
+        else if (pilihan == 4)
+        {
+            perkalian(matrix1, matrix2);
+        }
+
+        else if (pilihan == 5)
+        {
+        }
+
+        else
+        {
+            cout << "Pilihan tidak tersedia\n";
+        }
+
+    } while (pilihan != 5);
+
     return 0;
 }
