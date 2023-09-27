fe9ffde59a32e17b97036769a510f6f299fbd9d3
Shiro Kaslana <rifqiaryasaputra61@gmail.com>
2023-09-27 21:43:37 +0700

    HW 2

--- HW 2/Matrix.cpp
+++ HW 2/Matrix.cpp
@@ -2,22 +2,117 @@
 
 using namespace std;
 
-void pertambahan()
+void pertambahan(int matrix1[2][2], int matrix2[2][2])
 {
-}
-int main()
-{
-    int matrix1[2][2] = {{1, 3}, {2, 4}};
-    int matrix2[2][2] = {{1, 0}, {1, 0}};
+    int hasil[2][2];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            hasil[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
 
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 2; j++)
         {
-            cout << "matrix1[" << i << "][" << j << "]"
-                 << " = " << matrix1[i][j] << " \n";
+            cout << "Hasil[" << i << "][" << j << "]"
+                 << " = " << hasil[i][j] << " \n";
+        }
+    }
+}
+
+void perkurangan(int matrix1[2][2], int matrix2[2][2])
+{
+    int hasil[2][2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            hasil[i][j] = matrix1[i][j] - matrix2[i][j];
         }
     }
 
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << "Hasil[" << i << "][" << j << "]"
+                 << " = " << hasil[i][j] << " \n";
+        }
+    }
+}
+
+void perkalianInput(int matrix1[2][2])
+{
+    int input[2][2];
+    int hasil[2][2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cin >> input[i][j];
+        }
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            hasil[i][j] = 0;
+            for (int k = 0; k < 2; k++)
+            {
+                hasil[i][j] += matrix1[i][k] * input[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << "Hasil[" << i << "][" << j << "]"
+                 << " = " << hasil[i][j] << " \n";
+        }
+    }
+}
+
+void perkalian(int matrix1[2][2], int matrix2[2][2])
+{
+    int hasil[2][2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            hasil[i][j] = 0;
+            for (int k = 0; k < 2; k++)
+            {
+                hasil[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << "Hasil[" << i << "][" << j << "]"
+                 << " = " << hasil[i][j] << " \n";
+        }
+    }
+}
+
+int main()
+{
+    int matrix1[2][2] = {{1, 3}, {2, 4}};
+    int matrix2[2][2] = {{1, 1}, {0, 0}};
+
+    // perkurangan(matrix1, matrix2);
+    // perkalianInput(matrix1);
+    perkalian(matrix1, matrix2);
     return 0;
 }
