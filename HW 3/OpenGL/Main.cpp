2416735d9ae7ad23799d87f53c97dc07b36e18e8
Shiro Kaslana <rifqiaryasaputra61@gmail.com>
2023-10-11 20:05:01 +0700

    try

--- HW 3/OpenGL/Main.cpp
+++ HW 3/OpenGL/Main.cpp
@@ -1,17 +1,20 @@
 #include <GL/glut.h>
 #include <iostream>
 
-void drawLineDDA(float x1, float y1, float x2, float y2) {
+void drawLineDDA(float x1, float y1, float x2, float y2)
+{
     // Menghitung nilai delta
     float dx = x2 - x1;
     float dy = y2 - y1;
     float steps;
 
     // Menghitung jumlah step
-    if (abs(dx) > abs(dy)) {
+    if (abs(dx) > abs(dy))
+    {
         steps = abs(dx);
     }
-    else {
+    else
+    {
         steps = abs(dy);
     }
 
@@ -24,7 +27,8 @@ void drawLineDDA(float x1, float y1, float x2, float y2) {
     glColor3f(1.0, 0.0, 0.0);
 
     glBegin(GL_POINTS);
-    for (int i = 0; i < steps; i++) {
+    for (int i = 0; i < steps; i++)
+    {
         glVertex2f(x, y);
         x += xIncrement;
         y += yIncrement;
@@ -33,19 +37,28 @@ void drawLineDDA(float x1, float y1, float x2, float y2) {
     glFlush();
 }
 
-void display() {
+void display()
+{
     glClear(GL_COLOR_BUFFER_BIT);
 
-    drawLineDDA(50, 50, 200, 200);
+    float x1, y1, x2, y2;
+
+    // Prompt the user for input
+    std::cout << "Masukkan koordinat (x1 y1 x2 y2): ";
+    std::cin >> x1 >> y1 >> x2 >> y2;
+
+    // Call the DDA line drawing function with user-provided endpoints
+    drawLineDDA(x1, y1, x2, y2);
 
     glFlush();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(400, 400);
-    glutCreateWindow("DDA Line Drawing");
+    glutCreateWindow("DDA Garis");
     glOrtho(0, 400, 0, 400, -1, 1);
     glutDisplayFunc(display);
     glutMainLoop();
