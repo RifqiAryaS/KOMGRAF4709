e4371d12b22644e99ea780a9e7ea8f47ce4ab2a5
Shiro Kaslana <rifqiaryasaputra61@gmail.com>
2023-10-26 11:26:59 +0700

    Scaling and Rotation

--- HW 5/Rotasi/main.cpp
+++ HW 5/Rotasi/main.cpp
@@ -1,9 +1,32 @@
-#include <iostream>
+#include <GL/freeglut.h>
 
-using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+void display() {
+    // set warna background ke hitam
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Rotasi dengan 90 derajat
+    glRotatef(90.0,0.0f,0.0f,0.1f);
+
+    // Gambar garis
+    glBegin(GL_LINES);
+    glColor4f(1.0f, 1.0f, 1.0f, 0.0f);;
+    glVertex2f(0.2,0.3);
+    glVertex2f(0.5,0.3);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Rotation");
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+
     return 0;
 }
--- HW 5/Scaling/main.cpp
+++ HW 5/Scaling/main.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
+#include <GL/freeglut.h>
 
-using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+void display() {
+    // set warna background ke hitam
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // scaling (2,2)
+    glScalef(2.0f,2.0f,2.0f);
+
+    // Gambar garis
+    glBegin(GL_TRIANGLES);
+    glColor4f(1.0f, 1.0f, 1.0f, 0.0f);;
+    glVertex2f(0.2,0.3);
+    glVertex2f(0.5,0.3);
+    glVertex2f(0.35,0.0);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Scaling");
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+
     return 0;
 }
